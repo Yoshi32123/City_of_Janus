12cf4a9518eaccbc9014c6c75e790c11a13c4b64
Freddy Stock <fbs9594@rit.edu>
2018-09-28 01:21:10 -0400

    Basic attack script added
    player can now kill an enemy by walking over them

---
+++ Friend_Project/Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject manager;
+    private List<GameObject> enemyList;
+    [SerializeField]
+    private int damage;
+
+	// Use this for initialization
+	void Start () {
+        enemyList = manager.GetComponent<EnemyManager>().enemyList;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        attack();
+	}
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public void attack()
+    {
+        for(int i = 0; i < enemyList.Count; i++)
+        {
+            if (enemyList[i].GetComponent<Collider2D>().OverlapPoint(transform.position))
+            {
+                enemyList[i].GetComponent<EnemyData>().health -= damage;
+
+            }
+            
+            //if(CollisionDetection(new Rect(enemyList[i].transform.position, enemyList[i].GetComponent<Collider2D>().bounds.size)))
+            //{
+            //}
+           
+        }
+    }
+
+    bool CollisionDetection(Rect enemyRect)
+    {
+        
+        return (enemyRect.Contains(transform.position));
+
+    }
+}
