9908de193777eade361abeb6ff9bfe649a4f01b3
Freddy Stock <fbs9594@rit.edu>
2018-09-28 00:15:14 -0400

    Blocked out dungeon 1 floor 1. Added player movement.
    Dungeon 1 floor 1 for the goblin area is now blocked out using place holder sprites. The player character can be moved with WASD.

---
+++ Friend_Project/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+    private Vector3 direction;
+    private Vector3 velocity;
+    [SerializeField]
+    private float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Move();
+	}
+
+    /// <summary>
+    /// moves object based on keyboard inputs 
+    /// </summary>
+    void Move()
+    {
+        direction = new Vector3(0, 0);
+        if (Input.GetKey(KeyCode.W))
+        {
+            direction.y = 1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            direction.y = -1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            direction.x = 1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            direction.x = -1;
+        }
+        velocity = direction.normalized * speed;
+
+        transform.position += velocity;
+    }
+}
