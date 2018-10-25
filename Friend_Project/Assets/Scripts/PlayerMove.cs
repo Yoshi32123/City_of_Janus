2a87047dd1568ea35b3487b93334e2ca44600c2b
Freddy Stock <fbs9594@rit.edu>
2018-10-24 22:42:07 -0400

    Updated Scripts and Added Basic Stealth Scripts
    Made the move script no longer frame dependent. Added the ability for goblins to detect the player based on their distance from the player. How enemies detect the player will be reworked to be based on multiple factors and take into acount the terrain of the level.

--- Friend_Project/Assets/Scripts/PlayerMove.cs
+++ Friend_Project/Assets/Scripts/PlayerMove.cs
@@ -7,7 +7,7 @@ public class PlayerMove : MonoBehaviour {
     private Vector3 direction;
     private Vector3 velocity;
     [SerializeField]
-    private float speed;
+    private float speed; //defualt is 5
 
 	// Use this for initialization
 	void Start () {
@@ -27,22 +27,22 @@ public class PlayerMove : MonoBehaviour {
         direction = new Vector3(0, 0);
         if (Input.GetKey(KeyCode.W))
         {
-            direction.y = 1;
+            direction.y = speed;
         }
         if (Input.GetKey(KeyCode.S))
         {
-            direction.y = -1;
+            direction.y = -speed;
         }
         if (Input.GetKey(KeyCode.D))
         {
-            direction.x = 1;
+            direction.x = speed;
         }
         if (Input.GetKey(KeyCode.A))
         {
-            direction.x = -1;
+            direction.x = -speed;
         }
         velocity = direction.normalized * speed;
 
-        transform.position += velocity;
+        transform.position += velocity * Time.deltaTime;
     }
 }
