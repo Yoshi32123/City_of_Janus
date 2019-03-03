6a91f3a424b781c79aee99c922e987b0c7fdeeed
QuinnHop <quinnp.hopwood@gmail.com>
2019-03-02 23:29:31 -0500

    Checking if this works

---
+++ Friend_Project/Assets/Scripts/LoadScene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadScene : MonoBehaviour {
+
+    // Use this for initialization
+    public string sceneName;
+
+
+    void Start()
+    {
+        sceneName = "Main";
+    }
+    
+    public void OnClick()
+    {
+        SceneManager.LoadScene(sceneName);
+        Debug.Log(sceneName);
+    }
+}
